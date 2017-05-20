dffebbe340075fcc718326345ad79b221cb78398
Hubert Pyzik <hpyzik.dev@gmail.com>
2017-05-20 02:43:20 +0200

    #2 Remove example

--- coding-standards/07-blocks.js
+++ coding-standards/07-blocks.js
@@ -16,16 +16,6 @@
     // bad
     // if (test) test = false;
 
-    // good
-    var bar = function bar() {
-        return false;
-    };
-
-    // bad
-    // var foo = function foo() {
-    //   return false;
-    // };
-
     // * If you're using multi-line blocks with if and else, put else on the same line as your if block's closing brace.
 
     // good
